c9b61872237e9665b38d5b02d4a80317931df509
a.osetckiy <a.osetckiy@extracode.net>
2019-08-17 23:40:21 +0300

    добавил класс для чтения массива строк с файла

---
+++ src/main/java/ru/osetsky/search/utilities/ReaderFile.java
@@ -0,0 +1,32 @@
+package ru.osetsky.search.utilities;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReaderFile {
+
+    public static String[] readArraysOfStringFromFile(String filename) {
+        //определяем пустой массив изначально
+        String[] text = new String[0];
+        try {
+            String str = null;
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            while ((str = br.readLine()) != null) {
+                //получаем новые слова
+                String[] newWords = str.split(" ");
+                //создаем расширенный массив
+                String[] result = new String[text.length + newWords.length];
+                //копируем элементы в массив
+                System.arraycopy(text, 0, result, 0, text.length);
+                System.arraycopy(newWords, 0, result, text.length, newWords.length);
+                //присваиваем результирующий массив текущему
+                text = result;
+            }
+            br.close();
+        } catch (IOException exc) {
+            System.out.println("IO error!" + exc);
+        }
+        return text;
+    }
+}
