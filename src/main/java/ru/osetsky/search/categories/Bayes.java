131d5122c8c214d8327a20161ce98c918a2807b5
a.osetckiy <a.osetckiy@extracode.net>
2019-08-17 23:42:16 +0300

    добавил метод обучения групп по методу Байеса

--- src/main/java/ru/osetsky/search/categories/Bayes.java
+++ src/main/java/ru/osetsky/search/categories/Bayes.java
@@ -2,10 +2,46 @@ package ru.osetsky.search.categories;
 
 import de.daslaboratorium.machinelearning.classifier.Classifier;
 import de.daslaboratorium.machinelearning.classifier.bayes.BayesClassifier;
+import ru.osetsky.search.utilities.ReaderFile;
 
 import java.util.Arrays;
 
 public class Bayes {
+
+    public static void main(String[] args) {
+        // Create a new bayes classifier with string categories and string features.
+        Classifier<String, String> bayes = new BayesClassifier<String, String>();
+
+        // Two examples to learn from.
+        String[] music = ReaderFile.readArraysOfStringFromFile("music.txt");
+        String[] medic = ReaderFile.readArraysOfStringFromFile("medic.txt");
+
+        // Learn by classifying examples.
+        // New categories can be added on the fly, when they are first used.
+        // A classification consists of a category and a list of features
+        // that resulted in the classification in that category.
+        bayes.learn("music", Arrays.asList(music));
+        bayes.learn("medic", Arrays.asList(medic));
+
+        // Here are two unknown sentences to classify.
+        String[] unknownText1 = "today is a sunny day".split("\\s");
+        String[] unknownText2 = "there will be rain".split("\\s");
+
+        System.out.println( // will output "music"
+                bayes.classify(Arrays.asList(music)).getCategory());
+        System.out.println( // will output "medic"
+                bayes.classify(Arrays.asList(unknownText2)).getCategory());
+
+        // Get more detailed classification result.
+        ((BayesClassifier<String, String>) bayes).classifyDetailed(
+                Arrays.asList(unknownText1));
+
+        // Change the memory capacity. New learned classifications (using
+        // the learn method) are stored in a queue with the size given
+        // here and used to classify unknown sentences.
+        bayes.setMemoryCapacity(500);
+    }
+
     public void test() {
         // Create a new bayes classifier with string categories and string features.
         Classifier<String, String> bayes = new BayesClassifier<String, String>();
