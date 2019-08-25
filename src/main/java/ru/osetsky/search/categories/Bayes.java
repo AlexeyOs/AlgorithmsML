f30bb5c332bc635e67adf915cc0474493539594e
a.osetckiy <a.osetckiy@extracode.net>
2019-08-25 18:14:20 +0300

    перевел комментарии, создал класс для модификации метода Байеса

--- src/main/java/ru/osetsky/search/categories/Bayes.java
+++ src/main/java/ru/osetsky/search/categories/Bayes.java
@@ -1,7 +1,6 @@
 package ru.osetsky.search.categories;
 
-import de.daslaboratorium.machinelearning.classifier.Classifier;
-import de.daslaboratorium.machinelearning.classifier.bayes.BayesClassifier;
+import ru.osetsky.search.categories.modif.ModBayesClassifier;
 import ru.osetsky.search.utilities.ReaderFile;
 import ru.osetsky.search.utilities.Stemming;
 
@@ -10,19 +9,19 @@ import java.util.Arrays;
 public class Bayes {
 
     public static void main(String[] args) {
-        // Create a new bayes classifier with string categories and string features.
-        Classifier<String, String> bayes = new BayesClassifier<String, String>();
+        // Создан новый классификатор Байеса со строковыми категориями и строковыми элементами.
+        ModBayesClassifier<String, String> bayes = new ModBayesClassifier<String, String>();
 
-        // Two examples to learn from.
+        // Два примера для изучения.
         String[] music = ReaderFile.readArraysOfStringFromFile("music.txt");
         String[] medic = ReaderFile.readArraysOfStringFromFile("medic.txt");
 
         music = Stemming.setRusStemming(music);
         medic = Stemming.setRusStemming(medic);
-        // Learn by classifying examples.
-        // New categories can be added on the fly, when they are first used.
-        // A classification consists of a category and a list of features
-        // that resulted in the classification in that category.
+        // Учитесь, классифицируя примеры.
+        // Новые категории могут быть добавлены на лету, когда они впервые используются.
+        // Классификация состоит из категории и списка признаков
+        // что привело к классификации в этой категории.
         bayes.learn("music", Arrays.asList(music));
         bayes.learn("medic", Arrays.asList(medic));
 
@@ -33,52 +32,19 @@ public class Bayes {
         unknownText1 = Stemming.setRusStemming(unknownText1);
         unknownText2 = Stemming.setRusStemming(unknownText2);
 
-        System.out.println( // will output "music"
+        System.out.println( // будет вывод "music"
                 bayes.classify(Arrays.asList(unknownText1)).getCategory());
-        System.out.println( // will output "medic"
+        System.out.println( // будет вывод "medic"
                 bayes.classify(Arrays.asList(unknownText2)).getCategory());
 
-        // Get more detailed classification result.
-        ((BayesClassifier<String, String>) bayes).classifyDetailed(
-                Arrays.asList(unknownText1));
+        // Получить более подробный результат классификации.
+//        System.out.println(((BayesClassifier<String, String>) bayes).classifyDetailed(
+//                Arrays.asList(unknownText1)));
 
-        // Change the memory capacity. New learned classifications (using
-        // the learn method) are stored in a queue with the size given
-        // here and used to classify unknown sentences.
+        // Измените объем памяти. Новые изученные классификации (используя
+        // метод обучения) хранятся в очереди с заданным размером
+        // здесь и используется для классификации неизвестных предложений.
         bayes.setMemoryCapacity(500);
     }
 
-    public void test() {
-        // Create a new bayes classifier with string categories and string features.
-        Classifier<String, String> bayes = new BayesClassifier<String, String>();
-
-        // Two examples to learn from.
-        String[] positiveText = "I love sunny days".split("\\s");
-        String[] negativeText = "I hate rain".split("\\s");
-
-        // Learn by classifying examples.
-        // New categories can be added on the fly, when they are first used.
-        // A classification consists of a category and a list of features
-        // that resulted in the classification in that category.
-        bayes.learn("positive", Arrays.asList(positiveText));
-        bayes.learn("negative", Arrays.asList(negativeText));
-
-        // Here are two unknown sentences to classify.
-        String[] unknownText1 = "today is a sunny day".split("\\s");
-        String[] unknownText2 = "there will be rain".split("\\s");
-
-        System.out.println( // will output "positive"
-                bayes.classify(Arrays.asList(unknownText1)).getCategory());
-        System.out.println( // will output "negative"
-                bayes.classify(Arrays.asList(unknownText2)).getCategory());
-
-        // Get more detailed classification result.
-        ((BayesClassifier<String, String>) bayes).classifyDetailed(
-                Arrays.asList(unknownText1));
-
-        // Change the memory capacity. New learned classifications (using
-        // the learn method) are stored in a queue with the size given
-        // here and used to classify unknown sentences.
-        bayes.setMemoryCapacity(500);
-    }
 }
---
+++ src/main/java/ru/osetsky/search/categories/modif/ModBayesClassifier.java
@@ -0,0 +1,62 @@
+package ru.osetsky.search.categories.modif;
+
+import de.daslaboratorium.machinelearning.classifier.Classification;
+import de.daslaboratorium.machinelearning.classifier.Classifier;
+
+import de.daslaboratorium.machinelearning.classifier.Classification;
+import de.daslaboratorium.machinelearning.classifier.Classifier;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class ModBayesClassifier<T, K> extends Classifier<T, K> {
+    public ModBayesClassifier() {
+    }
+
+    private float featuresProbabilityProduct(Collection<T> features, K category) {
+        float product = 1.0F;
+
+        Object feature;
+        for(Iterator var4 = features.iterator(); var4.hasNext(); product *= this.featureWeighedAverage((T) feature, category)) {
+            feature = var4.next();
+        }
+
+        return product;
+    }
+
+    private float categoryProbability(Collection<T> features, K category) {
+        return (float)this.getCategoryCount(category) / (float)this.getCategoriesTotal() * this.featuresProbabilityProduct(features, category);
+    }
+
+    private SortedSet<Classification<T, K>> categoryProbabilities(Collection<T> features) {
+        SortedSet<Classification<T, K>> probabilities = new TreeSet(new Comparator<Classification<T, K>>() {
+            public int compare(Classification<T, K> o1, Classification<T, K> o2) {
+                int toReturn = Float.compare(o1.getProbability(), o2.getProbability());
+                if (toReturn == 0 && !o1.getCategory().equals(o2.getCategory())) {
+                    toReturn = -1;
+                }
+
+                return toReturn;
+            }
+        });
+        Iterator var3 = this.getCategories().iterator();
+
+        while(var3.hasNext()) {
+            K category = (K) var3.next();
+            probabilities.add(new Classification(features, category, this.categoryProbability(features, category)));
+        }
+
+        return probabilities;
+    }
+
+    public Classification<T, K> classify(Collection<T> features) {
+        SortedSet<Classification<T, K>> probabilites = this.categoryProbabilities(features);
+        return probabilites.size() > 0 ? (Classification)probabilites.last() : null;
+    }
+
+    public Collection<Classification<T, K>> classifyDetailed(Collection<T> features) {
+        return this.categoryProbabilities(features);
+    }
+}
